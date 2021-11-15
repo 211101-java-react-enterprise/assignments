d99b0471ec118c0b24d0ab1058ff4a64ceafc5c1
Wilson Neely <Naruisk.art@gmail.com>
2021-11-15 17:04:39 -0600

    ArrayList testing just in case :)

--- custom-collections/src/main/java/com/revature/custom_collections/collections/CollectionDriver.java
+++ custom-collections/src/main/java/com/revature/custom_collections/collections/CollectionDriver.java
@@ -7,6 +7,21 @@ public class CollectionDriver {
         // TODO: Provide a demonstration of your implemented data structures here.
         ArrayList<Integer> intList = new ArrayList<Integer>();
         intList.add(10);
+        intList.add(11);
+        intList.add(12);
+        intList.add(13);
+        intList.add(14);
+        intList.add(15);
+        intList.add(16);
         System.out.println(intList.get(0));
+        System.out.println(intList.get(6));
+        intList.add(17);
+        intList.add(18);
+        intList.add(19);
+        System.out.println(intList);
+        intList.add(20);
+        System.out.println(intList);
+        System.out.println(intList.get(9));
+        System.out.println(intList.get(10));
     }
 }
