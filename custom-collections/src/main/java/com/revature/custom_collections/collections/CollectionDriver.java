62d5294f88c23738628e3eb9a6d904d87b8597eb
QiZhang-UConn <Qi.Zhang@uconn.edu>
2021-11-16 11:10:07 -0500

    finalize for presentation

--- custom-collections/src/main/java/com/revature/custom_collections/collections/CollectionDriver.java
+++ custom-collections/src/main/java/com/revature/custom_collections/collections/CollectionDriver.java
@@ -5,14 +5,19 @@ public class CollectionDriver {
     public static void main(String[] args) {
         //___________________HashMap Test_____________________
         HashMap<String , String> hashMap = new HashMap<>();
-
+        System.out.println("___________HashMap Test______________");
         System.out.println("isEmpty: true="+hashMap.isEmpty());
         System.out.println( hashMap.put("a","1"));
         System.out.println( hashMap.put("b","2"));
         System.out.println( hashMap.put("c","3"));
         System.out.println( hashMap.put("d","4"));
         System.out.println("size: 4=" +hashMap.size());
-
+        //Null Insertion test
+        System.out.println( hashMap.put(null,"4"));
+        System.out.println( hashMap.put(null,null));
+        System.out.println( hashMap.get(null));
+        System.out.println("size 5="+hashMap.size());
+        //RemoveTest
         System.out.println( hashMap.get("b"));
         System.out.println( hashMap.remove("b"));
 
@@ -27,20 +32,19 @@ public class CollectionDriver {
         System.out.println( "contains value 9 false="+hashMap.containsValue("9"));
 
 
-        System.out.println( hashMap.put("b","2"));
 
-        System.out.println( hashMap.put(null,"4"));
-        System.out.println( hashMap.put(null,null));
-
-        System.out.println( hashMap.get("a"));
-        System.out.println( hashMap.get(null));
-
-        System.out.println("end");
 
         //_________________Has Set Test____________________________
+        System.out.println("___________________HashSet Test____________________");
         Set<String> set=new HashSet<>();
+        System.out.println("set is empty: true="+set.isEmpty());
+        System.out.println("added Hello_World");
         set.add("Hello_World");
-        System.out.println(set.isEmpty());
-        System.out.println(set.contains("Hello_World"));
+        System.out.println("set is empty: false="+set.isEmpty());
+        System.out.println("Contains Hello_World: true="+set.contains("Hello_World"));
+        System.out.println("duplicate add value Hello_World: false="+set.add("Hello_World"));
+        System.out.println("removed successfully: " +set.remove("Hello_World"));
+        System.out.println("Contains Hello_World: false="+set.contains("Hello_World"));
+
     }
 }
